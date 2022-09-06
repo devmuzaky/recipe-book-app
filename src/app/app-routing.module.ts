c0bd932b5e20bd9693e9cebee589d79a9d54f939
Muhammad Zaky <Devmuzaky@gmail.com>
2022-09-06 10:56:48 +0200

    add authentication component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,19 @@ import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
 import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
+import {RecipeResolverService} from "./recipes/recipe-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch:"full"},
   { path: 'recipes', component: RecipesComponent, children: [
       { path: '', component:RecipeStartComponent},
       { path:'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent},
-      { path:':id/edit', component: RecipeEditComponent }
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService]},
+      { path:':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] }
     ]},
-  {path: 'shopping-list', component: ShoppingListComponent}
+  {path: 'shopping-list', component: ShoppingListComponent},
+  { path: 'auth', component: AuthComponent}
 
 ]
 
